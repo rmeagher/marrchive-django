d7718c9e17decdd8b232b36c8746f377c564626f
Ryan Meagher <Meagher-Ryan@norc.org>
2020-03-05 10:23:20 -0600

    better urls

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from api import views
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'books/?', views.BookViewSet)
-router.register(r'authors', views.AuthorViewSet)
+router.register(r'authors/?', views.AuthorViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 apiurlpatterns = [
-    path('api/', include(router.urls)),
+    path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
